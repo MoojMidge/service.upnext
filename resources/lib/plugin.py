5aba43c92936033632e958372806be7f2f219cb8
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2022-12-30 13:17:00 +1100

    Partially revert cfa5151
    Combined tvshow and movie widgets intended to display poster artwork
    Only combined episode (lanscape/thumb artwork) and movie (poster artwork) widgets require artwork fallbacks

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -247,14 +247,12 @@ def generate_watched_media_list(addon_handle, addon_id, **kwargs):  # pylint: di
         limit=SETTINGS.widget_list_limit,
         sort=api.SORT_LASTPLAYED,
         filters=api.FILTER_WATCHED,
-        process_art=True,
     )
     tvshows = api.get_videos_from_library(
         media_type='tvshows',
         limit=SETTINGS.widget_list_limit,
         sort=api.SORT_LASTPLAYED,
         filters=api.FILTER_WATCHED,
-        process_art=True,
     )
 
     videos = utils.merge_iterable(
