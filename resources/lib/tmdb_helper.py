02ffc4814e4185d60d6fc71dcae502fef7efaf7f
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2023-01-28 21:26:57 +1100

    Update TMDB Helper integration

--- resources/lib/tmdb_helper.py
+++ resources/lib/tmdb_helper.py
@@ -3,6 +3,8 @@
 
 from __future__ import absolute_import, division, unicode_literals
 
+from importlib.util import find_spec, module_from_spec
+from sys import modules
 try:
     from urllib.parse import urlencode
 except ImportError:
@@ -19,7 +21,9 @@ def log(msg, level=utils.LOGERROR):
 
 
 class Import(object):  # pylint: disable=too-few-public-methods
-    def __new__(cls, object_name=None):
+    def __new__(cls, mod_name=None, obj_name=None,
+                mod_attrs=None, pre_mod_attrs=None, post_mod_attrs=None,
+                obj_attrs=None):
         def substitute(cls, func=None, default_return=None):
             def wrapper(*_args, **_kwargs):
                 return default_return
@@ -38,29 +42,65 @@ class Import(object):  # pylint: disable=too-few-public-methods
             return cls._initialised
 
         try:
-            module_name = 'themoviedb_helper'
-            module = __import__(module_name, fromlist=[object_name])
-            imported_object = getattr(module, object_name)
+            spec = find_spec(f'themoviedb_helper.{mod_name}')
+            module = module_from_spec(spec)
+            if spec.name not in modules:
+                modules[spec.name] = module
+            attrs = None
+            if not pre_mod_attrs:
+                if mod_attrs:
+                    attrs = mod_attrs
+            elif mod_attrs:
+                attrs = dict(mod_attrs, **pre_mod_attrs)
+            else:
+                attrs = pre_mod_attrs
+            if attrs:
+                module.__dict__.update(attrs)
+            spec.loader.exec_module(module)
+            attrs = None
+            if not post_mod_attrs:
+                if mod_attrs:
+                    attrs = mod_attrs
+            elif mod_attrs:
+                attrs = dict(mod_attrs, **post_mod_attrs)
+            else:
+                attrs = post_mod_attrs
+            if attrs:
+                module.__dict__.update(attrs)
+            if obj_name:
+                imported_obj = getattr(module, obj_name)
+                del modules[spec.name]
             initialised = True
         except (AttributeError, ImportError):
             from traceback import format_exc
-            log('ImportError: {0}.{1}\n{2}'.format(module_name, object_name,
-                                                   format_exc()))
-            imported_object = object
+            log('ImportError: {0}'.format(format_exc()))
+            module = None
+            imported_obj = object
             initialised = False
 
-        if isinstance(imported_object, type):
-            return type(object_name, (imported_object,), {
+        if isinstance(imported_obj, type):
+            _dict = {
                 '_initialised': initialised,
                 '_substitute': classmethod(substitute),
                 'is_initialised': classmethod(is_initialised),
-            })
-        return imported_object
+            }
+            if obj_attrs is not None:
+                _dict.update(obj_attrs)
+            return type(obj_name, (imported_obj, ), _dict)
+
+        if imported_obj:
+            if obj_attrs is not None:
+                imported_obj.__dict__.update(obj_attrs)
+            return imported_obj
+
+        return module
 
 
-TMDb = Import('TMDb')  # pylint: disable=invalid-name
-Players = Import('Players')  # pylint: disable=invalid-name
-get_next_episodes = Import('get_next_episodes')  # pylint: disable=invalid-name
+TMDb = Import('api.tmdb.api', 'TMDb', obj_attrs={'api_key': 'b5004196f5004839a7b0a89e623d3bd2'})  # pylint: disable=invalid-name
+get_next_episodes = Import('player.details', 'get_next_episodes', mod_attrs={'TMDb': TMDb})  # pylint: disable=invalid-name
+get_item_details = Import('player.details', 'get_item_details', mod_attrs={'TMDb': TMDb})  # pylint: disable=invalid-name
+Players = Import('player.players', 'Players', mod_attrs={'TMDb': TMDb, 'get_item_details': get_item_details})  # pylint: disable=invalid-name
+make_playlist = Import('player.putils', 'make_playlist')  # pylint: disable=invalid-name
 
 
 class TMDB(TMDb):  # pylint: disable=inherit-non-class,too-few-public-methods
@@ -81,7 +121,7 @@ class Player(Players):  # pylint: disable=inherit-non-class,too-few-public-metho
     @Players._substitute  # pylint: disable=no-member
     def __init__(self, **kwargs):
         if 'tmdb_id' not in kwargs:
-            kwargs['tmdb_id'] = TMDb().get_tmdb_id(**kwargs)  # pylint: disable=not-callable
+            kwargs['tmdb_id'] = TMDB().get_tmdb_id(**kwargs)  # pylint: disable=not-callable
         super(Player, self).__init__(**kwargs)
         self._success = False
 
@@ -122,14 +162,16 @@ class Player(Players):  # pylint: disable=inherit-non-class,too-few-public-metho
         if not player:
             return None
         episodes = get_next_episodes(self.tmdb_id, self.season, self.episode,  # pylint: disable=not-callable
-                                     player.get('file'))
+                                     player.get('file')) or []
+        for episode in episodes:
+            episode.path = 'plugin://service.upnext/play_plugin'
         return episodes
 
     def queue(self):
-        player = self.current_player
-        self.current_player = self.get_default_player()  # pylint: disable=attribute-defined-outside-init
-        self.queue_next_episodes(route='make_playlist')
-        self.current_player = player  # pylint: disable=attribute-defined-outside-init
+        episodes = self.get_next_episodes()
+        if not episodes or len(episodes) < 2:
+            return
+        make_playlist(episodes)
 
     @Players._substitute  # pylint: disable=no-member
     def select_player(self, *args, **kwargs):
