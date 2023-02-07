MoojMidge/service.upnext

commit ac86fd03305448487a0f134e8443f1a49c1377f9

Expand core UpNext functionality to include movies from sets

resources/lib/state.py
@@ -19,11 +19,6 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
         'current_item',
         'filename',
         'total_time',
-        'playcount',
-        'tvshowid',
-        'episodeid',
-        'episode_number',
-        'season_identifier',
         # Popup state variables
         'next_item',
         'popup_time',
@@ -46,14 +41,17 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
         self.data = None
         self.encoding = 'base64'
         # Current video details
-        self.current_item = None
+        self.current_item = {
+            'details': {},
+            'source': None,
+            'media_type': None,
+            'db_id': constants.UNDEFINED,
+            'group_id': constants.UNDEFINED,
+            'group_name': None,
+            'group_idx': constants.UNDEFINED,
+        }
         self.filename = None
         self.total_time = 0
-        self.playcount = 0
-        self.tvshowid = constants.UNDEFINED
-        self.episodeid = constants.UNDEFINED
-        self.episode_number = None
-        self.season_identifier = None
         # Popup state variables
         self.next_item = None
         self.popup_time = 0
@@ -102,57 +100,54 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
     def get_next(self):
         """Get next video to play, based on current video source"""
 
-        next_item = None
+        next_video = None
         source = None
+        media_type = self.current_item['media_type']
         playlist_position = api.get_playlist_position()
         plugin_type = self.get_plugin_type(playlist_position)
 
         # Next episode from plugin data
         if plugin_type:
-            next_item = self.data.get('next_episode')
+            next_video = self.data.get('next_episode')
             source = constants.PLUGIN_TYPES[plugin_type]
 
             if (SETTINGS.unwatched_only
-                    and utils.get_int(next_item, 'playcount') > 0):
-                next_item = None
-            self.log('Plugin next_episode: {0}'.format(next_item))
+                    and utils.get_int(next_video, 'playcount') > 0):
+                next_video = None
+            self.log('Plugin next_episode: {0}'.format(next_video))
 
         # Next item from non-plugin playlist
         elif playlist_position and not self.shuffle_on:
-            next_item = api.get_from_playlist(
+            next_video = api.get_from_playlist(
                 position=playlist_position,
                 properties=(api.EPISODE_PROPERTIES | api.MOVIE_PROPERTIES),
                 unwatched_only=SETTINGS.unwatched_only
             )
+            media_type = constants.UNDEFINED
             source = 'playlist'
 
-        # Next episode from Kodi library
+        # Next video from Kodi library
         else:
-            current_item = api.get_from_library(
-                self.episodeid,
-                self.tvshowid
-            )
-            next_item, new_season = api.get_next_from_library(
-                episode=current_item,
-                tvshowid=self.tvshowid,
-                unwatched_only=SETTINGS.unwatched_only,
+            next_video = api.get_next_from_library(
+                item=self.current_item,
                 next_season=SETTINGS.next_season,
+                unwatched_only=SETTINGS.unwatched_only,
                 random=self.shuffle_on
             )
             source = 'library'
-            # Show Still Watching? popup if next episode is from next season
-            if new_season:
+            # Show Still Watching? popup if next episode is from next season or
+            # next item is a movie
+            if media_type == 'movie' or (
+                    not self.shuffle_on and next_video and
+                    next_video['season']
+                    != self.current_item['details']['season']
+            ):
                 self.played_in_a_row = SETTINGS.played_limit
 
-        if next_item and source:
-            self.next_item = {
-                'details': next_item,
-                'source': source
-            }
-        if not self.next_item:
-            return None, None
-
-        return self.next_item['details'], self.next_item['source']
+        self.next_item = utils.create_item_details(
+            next_video, source, media_type, playlist_position
+        )
+        return self.next_item
 
     def get_detect_time(self):
         return self.detect_time
@@ -241,11 +236,11 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
 
     def process_now_playing(self, playlist_position, plugin_type, media_type):
         if plugin_type:
-            current_item = self._get_plugin_now_playing()
+            current_video = self._get_plugin_now_playing(media_type)
             source = constants.PLUGIN_TYPES[plugin_type]
 
         elif playlist_position:
-            current_item = api.get_from_playlist(
+            current_video = api.get_from_playlist(
                 position=(playlist_position - 1),
                 properties=(
                     api.MOVIE_PROPERTIES if media_type == 'movie' else
@@ -254,53 +249,50 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
             )
             source = 'playlist'
 
-            if current_item:
-                if not current_item.get('showtitle'):
-                    current_item['showtitle'] = constants.MIXED_PLAYLIST
-                if not current_item['season']:
-                    current_item['season'] = 0
-                if not current_item['episode']:
-                    current_item['episode'] = playlist_position
-
         elif media_type in ('episode', 'movie'):
-            current_item = self._get_library_now_playing(media_type)
+            current_video = self._get_library_now_playing(media_type)
             source = 'library'
 
         else:
-            current_item = None
+            current_video = None
             source = None
 
-        if current_item and source:
-            self.current_item = {
-                'details': current_item,
-                'source': source
-            }
-        if not self.current_item:
+        if not current_video or not source:
             return None
 
-        tvshowid = self.get_tvshowid(self.current_item['details'])
-        # Reset played in a row count if new show playing
-        if self.tvshowid != tvshowid:
-            self.log('Reset played count: tvshowid change - {0} to {1}'.format(
-                self.tvshowid, tvshowid
-            ))
+        current_item = utils.create_item_details(
+            current_video, source, media_type, playlist_position
+        )
+
+        # Reset played in a row count if new tvshow or set is playing, unless
+        # playing from a playlist
+        if (not playlist_position and self.current_item
+                and self.current_item['group_id'] != current_item['group_id']):
+            self.log(
+                'Reset played count: {0} group_id changed - {1} to {2}'.format(
+                    media_type,
+                    self.current_item['group_id'],
+                    current_item['group_id']
+                )
+            )
             self.played_in_a_row = 1
 
-        self._set_tvshowid()
-        self._set_episodeid()
-        self._set_episode_number()
-        self._set_playcount()
-        self._set_season_identifier()
+        self.current_item = current_item
+        return self.current_item
 
-        return self.current_item['details']
-
-    def _get_plugin_now_playing(self):
+    def _get_plugin_now_playing(self, media_type):
         if self.data:
             # Fallback to now playing info if plugin does not provide current
             # episode details
             current_item = (
                 self.data.get('current_episode')
-                or api.get_now_playing(retry=SETTINGS.api_retry_attempts)
+                or api.get_now_playing(
+                    properties=(
+                        api.MOVIE_PROPERTIES if media_type == 'movie' else
+                        api.EPISODE_PROPERTIES
+                    ),
+                    retry=SETTINGS.api_retry_attempts
+                )
             )
         else:
             current_item = None
@@ -326,7 +318,7 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
             return current_item
 
         # Get current tvshowid or search in library if detail missing
-        tvshowid = self.get_tvshowid(current_item)
+        tvshowid = current_item.get('tvshowid', constants.UNDEFINED)
         if tvshowid == constants.UNDEFINED:
             tvshowid = api.get_tvshowid(current_item.get('showtitle'))
             self.log('Fetched tvshowid: {0}'.format(tvshowid))
@@ -335,8 +327,11 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
             return None
         current_item['tvshowid'] = tvshowid
 
-        # Get current episodeid or search in library if detail missing
-        episodeid = self.get_episodeid(current_item)
+        # Get current episode id or search in library if detail missing
+        episodeid = (
+            utils.get_int(current_item, 'episodeid', None)
+            or utils.get_int(current_item, 'id')
+        )
         if episodeid == constants.UNDEFINED:
             episodeid = api.get_episodeid(
                 tvshowid,

.pylintrc
@@ -11,5 +11,6 @@ disable=
     useless-object-inheritance,
     super-on-old-class,
     super-with-arguments,
-    too-many-instance-attributes
+    too-many-instance-attributes,
+    too-many-lines
     

resources/lib/api.py
@@ -327,33 +327,26 @@ def get_item_id(item):
     }
 
 
-def play_kodi_item(episode, resume=False):
+def play_kodi_item(item, resume=False):
     """Function to directly play a file from the Kodi library"""
 
-    log('Playing from library: {0}'.format(episode))
+    log('Playing from library: {0}'.format(item))
+
+    item = get_item_id(item)
     utils.jsonrpc(
         method='Player.Open',
-        params={'item': {'episodeid': utils.get_int(episode, 'episodeid')}},
+        params={'item': item},
         options={'resume': resume},
         no_response=True
     )
 
 
-def queue_next_item(data=None, episode=None):
-    """Function to add next episode to the UpNext queue"""
+def queue_next_item(data=None, item=None):
+    """Function to add next video to the UpNext queue"""
 
-    next_item = {}
-    play_url = data.get('play_url') if data else None
-    episodeid = (
-        utils.get_int(episode, 'episodeid') if episode
-        else constants.UNDEFINED
-    )
-
-    if play_url:
-        next_item.update(file=play_url)
-
-    elif episode and episodeid != constants.UNDEFINED:
-        next_item.update(episodeid=episodeid)
+    next_item = {
+        'file': data['play_url']
+    } if data and 'play_url' in data else get_item_id(item)
 
     if next_item:
         log('Adding to queue: {0}'.format(next_item))
@@ -479,9 +472,9 @@ def get_from_playlist(position, properties, unwatched_only=False):
     # Try and populate required details if missing
     if not item.get('title'):
         item['title'] = item.get('label', '')
-    item['episodeid'] = utils.get_int(
-        item, 'episodeid',
-        utils.get_int(item, 'id')
+    item['episodeid'] = (
+        utils.get_int(item, 'episodeid', None)
+        or utils.get_int(item, 'id')
     )
     item['tvshowid'] = utils.get_int(item, 'tvshowid')
     # If missing season/episode, change to empty string to avoid episode
@@ -639,19 +632,32 @@ def get_now_playing(properties, retry=3):
     return result
 
 
-def get_next_from_library(episode=constants.UNDEFINED,
-                          tvshowid=None,
-                          unwatched_only=False,
-                          next_season=True,
-                          random=False):
+def get_next_from_library(item, **kwargs):
+    """Function to get details of next episode in tvshow, or next movie in
+    movieset, from Kodi library"""
+
+    if item['media_type'] == 'episode':
+        return get_next_episode_from_library(episode=item['details'], **kwargs)
+
+    if item['media_type'] == 'movie':
+        if 'next_season' in kwargs:
+            del kwargs['next_season']
+        return get_next_movie_from_library(movie=item['details'], **kwargs)
+
+    log('No next video found, unsupported media type', utils.LOGWARNING)
+    return None
+
+
+def get_next_episode_from_library(episode=constants.UNDEFINED,
+                                  next_season=True,
+                                  unwatched_only=False,
+                                  random=False):
     """Function to get show and next episode details from Kodi library"""
 
     if not episode or episode == constants.UNDEFINED:
         log('No next episode found, current episode not in library',
             utils.LOGWARNING)
-        episode = None
-        new_season = False
-        return episode, new_season
+        return episode
 
     (path, filename) = os.path.split(episode['file'])
     _FILTER_NOT_FILE['value'] = filename
@@ -669,7 +675,10 @@ def get_next_from_library(episode=constants.UNDEFINED,
         # Exclude watched episodes
         filters.append(_FILTER_UNWATCHED)
 
-    if not random:
+    if random:
+        sort = _SORT_RANDOM
+    else:
+        sort = _SORT_EPISODE
         current_season = str(episode['season'])
         _FILTER_THIS_SEASON['value'] = current_season
         _FILTER_NEXT_SEASON['value'] = current_season
@@ -682,18 +691,12 @@ def get_next_from_library(episode=constants.UNDEFINED,
 
     filters = {'and': filters}
 
-    if not tvshowid:
-        tvshowid = episode.get('tvshowid', constants.UNDEFINED)
-
     result = utils.jsonrpc(
         method='VideoLibrary.GetEpisodes',
         params={
-            'tvshowid': tvshowid,
+            'tvshowid': episode.get('tvshowid', constants.UNDEFINED),
             'properties': EPISODE_PROPERTIES,
-            'sort': (
-                _SORT_RANDOM if random
-                else _SORT_EPISODE
-            ),
+            'sort': sort,
             'limits': _QUERY_LIMIT_ONE,
             'filter': filters
         }
@@ -702,18 +705,15 @@ def get_next_from_library(episode=constants.UNDEFINED,
 
     if not result:
         log('No next episode found in library')
-        episode = None
-        new_season = False
-        return episode, new_season
+        return None
 
     log('Next episode from library: {0}'.format(result[0]))
-    new_season = not random and episode['season'] != result[0]['season']
     # Update current episode details dict, containing tvshow details, with next
     # episode details. Surprisingly difficult to retain backwards compatibility
     # episode = episode | result[0]       # Python > v3.9
     # episode = {**episode, **result[0]}  # Python > v3.5
     episode = dict(episode, **result[0])  # Python > v2.7
-    return episode, new_season
+    return episode
 
 
 def get_next_movie_from_library(movie=constants.UNDEFINED,
@@ -770,41 +770,36 @@ def get_next_movie_from_library(movie=constants.UNDEFINED,
     return movie
 
 
-def get_from_library(episodeid, tvshowid=None):
-    """Function to get show and episode details from Kodi library"""
+def get_from_library(media_type=None, db_id=constants.UNDEFINED, item=None):
+    """Function to get video and collection details from Kodi library"""
 
-    result = utils.jsonrpc(
-        method='VideoLibrary.GetEpisodeDetails',
-        params={
-            'episodeid': episodeid,
-            'properties': EPISODE_PROPERTIES
-        }
-    )
-    result = result.get('result', {}).get('episodedetails')
+    if item:
+        media_type = item['media_type']
+        db_id = item['db_id']
+
+    if not media_type or db_id == constants.UNDEFINED:
+        log('Video info not found in library, invalid dbid', utils.LOGWARNING)
+        return None
+
+    result, _ = get_details_from_library(media_type=media_type, db_id=db_id)
 
     if not result:
-        log('Episode info not found in library', utils.LOGWARNING)
+        log('Video info not found in library', utils.LOGWARNING)
         return None
-    episode = result
 
-    if not tvshowid:
-        tvshowid = episode.get('tvshowid', constants.UNDEFINED)
+    if media_type == 'episode':
+        tvshow_details, _ = get_details_from_library(
+            media_type='tvshow',
+            db_id=result.get('tvshowid', constants.UNDEFINED)
+        )
 
-    result = utils.jsonrpc(
-        method='VideoLibrary.GetTVShowDetails',
-        params={
-            'tvshowid': tvshowid,
-            'properties': TVSHOW_PROPERTIES
-        }
-    )
-    result = result.get('result', {}).get('tvshowdetails')
+        if not tvshow_details:
+            log('Show info not found in library', utils.LOGWARNING)
+            return None
+        tvshow_details.update(result)
+        result = tvshow_details
 
-    if not result:
-        log('Show info not found in library', utils.LOGWARNING)
-        return None
-    result.update(episode)
-
-    log('Episode from library: {0}'.format(result))
+    log('Info from library: {0}'.format(result))
     return result
 
 
@@ -884,18 +879,12 @@ def get_details_from_library(media_type=None,
     return result, detail_type
 
 
-def handle_just_watched(episodeid, playcount,
-                        reset_playcount=False, reset_resume=True):
+def handle_just_watched(item, reset_playcount=False, reset_resume=True):
     """Function to update playcount and resume point of just watched video"""
 
-    result = utils.jsonrpc(
-        method='VideoLibrary.GetEpisodeDetails',
-        params={
-            'episodeid': episodeid,
-            'properties': ['playcount', 'resume'],
-        }
+    result, detail_type = get_details_from_library(
+        item=item, properties=['playcount', 'resume']
     )
-    result = result.get('result', {}).get('episodedetails')
 
     if result:
         actual_playcount = utils.get_int(result, 'playcount', 0)
@@ -918,15 +907,15 @@ def handle_just_watched(episodeid, playcount,
 
     # Only update library if playcount or resume point needs to change
     if params:
-        params['episodeid'] = episodeid
+        params[detail_type['db_id']] = item['db_id']
         utils.jsonrpc(
-            method='VideoLibrary.SetEpisodeDetails',
+            method=detail_type['set_method'],
             params=params,
             no_response=True
         )
 
-    log('Library update: id - {0}{1}{2}{3}'.format(
-        episodeid,
+    log('Library update: {0}{1}{2}{3}'.format(
+        '{0}_id - {1}'.format(item['media_type'], item['db_id']),
         ', playcount - {0} to {1}'.format(actual_playcount, playcount)
         if 'playcount' in params else '',
         ', resume - {0} to 0'.format(actual_resume)

resources/lib/constants.py
@@ -52,6 +52,7 @@ LOG_ENABLE_DISABLED = 0
 LOG_ENABLE_INFO = 1
 LOG_ENABLE_DEBUG = 2
 
+UNTITLED_SHOW = '_untitled'
 MIXED_PLAYLIST = '_mixedplaylist'
 
 PLUGIN_TYPES = (

resources/lib/detector.py
@@ -22,8 +22,8 @@ class UpNextHashStore(object):
     __slots__ = (
         'version',
         'hash_size',
-        'seasonid',
-        'episode_number',
+        'group_name',
+        'group_idx',
         'data',
         'timestamps'
     )
@@ -31,12 +31,11 @@ class UpNextHashStore(object):
     def __init__(self, **kwargs):
         self.version = kwargs.get('version', 0.2)
         self.hash_size = kwargs.get('hash_size', (8, 8))
-        self.seasonid = kwargs.get('seasonid', '')
-        self.episode_number = kwargs.get(
-            'episode_number', constants.UNDEFINED
-        )
+        item = kwargs.get('item', {})
+        self.group_name = item.get('group_name', '')
+        self.group_idx = item.get('group_idx', constants.UNDEFINED)
         self.data = kwargs.get('data', {})
-        self.timestamps = kwargs.get('timestamps', {self.episode_number: None})
+        self.timestamps = kwargs.get('timestamps', {self.group_idx: None})
 
     @staticmethod
     def int_to_hash(val, hash_size):
@@ -56,29 +55,37 @@ class UpNextHashStore(object):
     def log(cls, msg, level=utils.LOGDEBUG):
         utils.log(msg, name=cls.__name__, level=level)
 
-    def is_valid(self, seasonid=None, episode_number=None, for_saving=False):
+    def is_valid(self, item=None, for_saving=False):
+
+        if item:
+            group_name = item.get('group_name')
+            group_idx = item.get('group_idx')
+        else:
+            group_name = None
+            group_idx = None
+
         # Non-episodic video is being played
-        if not self.seasonid or self.episode_number == constants.UNDEFINED:
+        if not self.group_name or self.group_idx == constants.UNDEFINED:
             return False
 
         # Playlist with no episode details
-        if for_saving and self.seasonid.startswith(constants.MIXED_PLAYLIST):
+        if for_saving and self.group_name.startswith(constants.MIXED_PLAYLIST):
             return False
 
         # No new episode details, assume current hashes are still valid
-        if seasonid is None and episode_number is None:
+        if group_name is None and group_idx is None:
             return True
 
         # Current episode matches, current hashes are still valid
-        if self.seasonid == seasonid and self.episode_number == episode_number:
+        if self.group_name == group_name and self.group_idx == group_idx:
             return True
 
         # New video is being played, invalidate old hashes
         return False
 
     def invalidate(self):
-        self.seasonid = ''
-        self.episode_number = constants.UNDEFINED
+        self.group_name = ''
+        self.group_idx = constants.UNDEFINED
 
     def load(self, identifier):
         target = file_utils.get_legal_filename(
@@ -105,9 +112,9 @@ class UpNextHashStore(object):
             }
         if 'timestamps' in hashes:
             self.timestamps = {
-                utils.get_int(episode_number):
-                    hashes['timestamps'][episode_number]
-                for episode_number in hashes['timestamps']
+                utils.get_int(group_idx):
+                    hashes['timestamps'][group_idx]
+                for group_idx in hashes['timestamps']
             }
 
         self.log('Hashes loaded from {0}'.format(target))
@@ -570,7 +577,7 @@ class UpNextDetector(object):
 
         self.hash_index = {
             # Hash indexes are tuples containing the following data:
-            # (time_to_end, time_from_start, episode_number)
+            # (time_to_end, time_from_start, group_idx)
             # Current hash
             'current': (0, 0, 0),
             # Previous hash
@@ -590,11 +597,10 @@ class UpNextDetector(object):
         # Round down width to multiple of 2
         hash_size[0] = int(hash_size[0] - hash_size[0] % 2)
 
-        # Hashes for currently playing episode
+        # Hashes for currently playing item
         self.hashes = UpNextHashStore(
             hash_size=hash_size,
-            seasonid=self.state.get_season_identifier(),
-            episode_number=self.state.get_episode_number(),
+            item=self.state.current_item,
             # Representative hash of centred end credits text on a dark
             # background stored as first hash. Masked significance weights
             # stored as second hash.
@@ -616,7 +622,7 @@ class UpNextDetector(object):
         # Hashes from previously played episodes
         self.past_hashes = UpNextHashStore(hash_size=hash_size)
         if SETTINGS.detector_save_path and self.hashes.is_valid():
-            self.past_hashes.load(self.hashes.seasonid)
+            self.past_hashes.load(self.hashes.group_name)
 
         # Number of consecutive frame matches required for a positive detection
         # Set to 5s of captured frames as default
@@ -729,7 +735,7 @@ class UpNextDetector(object):
                 self.hash_index['current'] = (
                     int(self.player.getTotalTime() - play_time),
                     int(play_time),
-                    self.hashes.episode_number
+                    self.hashes.group_idx
                 )
                 # Only capture if playing at normal speed
                 check_fail = self.player.get_speed() < 1
@@ -847,7 +853,7 @@ class UpNextDetector(object):
 
     def reset(self):
         self._hash_match_reset()
-        self.hashes.timestamps[self.hashes.episode_number] = None
+        self.hashes.timestamps[self.hashes.group_idx] = None
         self.hash_index['detected_at'] = None
 
     def start(self, restart=False):
@@ -857,15 +863,12 @@ class UpNextDetector(object):
             self.stop()
 
         # Reset detector data if episode has changed
-        if not self.hashes.is_valid(
-                self.state.get_season_identifier(),
-                self.state.get_episode_number()
-        ):
+        if not self.hashes.is_valid(item=self.state.current_item):
             self._init_hashes()
 
         # If a previously detected timestamp exists then use it
         stored_timestamp = self.past_hashes.timestamps.get(
-            self.hashes.episode_number
+            self.hashes.group_idx
         )
         if stored_timestamp and not SETTINGS.detector_debug:
             self.log('Stored credits timestamp found')
@@ -943,7 +946,7 @@ class UpNextDetector(object):
         ) if self.match_counts['detected'] else self.hashes.data)
 
         if SETTINGS.detector_save_path:
-            self.past_hashes.save(self.hashes.seasonid)
+            self.past_hashes.save(self.hashes.group_name)
 
     def update_timestamp(self, play_time):
         # Timestamp already stored or credits not detected
@@ -953,6 +956,6 @@ class UpNextDetector(object):
         with self._lock:
             self.log('Credits detected')
             self.hash_index['detected_at'] = self.hash_index['current']
-            self.hashes.timestamps[self.hashes.episode_number] = play_time
+            self.hashes.timestamps[self.hashes.group_idx] = play_time
             self.state.set_detected_popup_time(play_time)
             utils.event('upnext_credits_detected')

resources/lib/monitor.py
@@ -175,8 +175,7 @@ class UpNextMonitor(xbmc.Monitor, object):
         # Update playcount and reset resume point of previous file
         if self.state.playing_next and SETTINGS.mark_watched:
             api.handle_just_watched(
-                episodeid=self.state.get_episodeid(),
-                playcount=self.state.get_playcount(),
+                item=self.state.current_item,
                 reset_playcount=(
                     SETTINGS.mark_watched == constants.SETTING_OFF
                 ),

commit 20d22254b8a1c85c7c60a41a20b412c5ae2e4c9a

Fix bug where episode info was missing tvshow info
- Partial regression caused by ac86fd03305448487a0f134e8443f1a49c1377f9

resources/lib/state.py
@@ -283,15 +283,17 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
 
     @classmethod
     def _get_library_now_playing(cls, play_info):  # pylint: disable=too-many-branches, too-many-return-statements
-        media_type = play_info.get('type')
-        current_video = api.get_now_playing(
-            properties=api.get_json_properties(play_info, {'mediapath'}),
-            retry=SETTINGS.api_retry_attempts
-        )
+        if 'id' in play_info:
+            current_video = api.get_from_library(item=play_info)
+        else:
+            current_video = api.get_now_playing(
+                properties=api.get_json_properties(play_info, {'mediapath'}),
+                retry=SETTINGS.api_retry_attempts
+            )
         if not current_video:
             return None
 
-        if media_type == 'movie':
+        if current_video['type'] == 'movie':
             return (
                 current_video if utils.get_int(current_video, 'setid') > 0
                 else None