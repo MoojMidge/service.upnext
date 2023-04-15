85a92b47dc29d8fe696370b528bf8a78e4c060a4
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2023-04-16 01:23:46 +1000

    Modify check for invalid setid
    - Better matches real library values
    - Allows use of dummy data for testing

--- resources/lib/api.py
+++ resources/lib/api.py
@@ -831,7 +831,8 @@ def get_next_movie_from_library(movie=constants.UNDEFINED,
             utils.LOGWARNING)
         return None
 
-    if utils.get_int(movie, 'setid') <= 0:
+    setid = utils.get_int(movie, 'setid')
+    if not setid or setid == constants.UNDEFINED:
         log('No next movie found, invalid movie setid', utils.LOGWARNING)
         return None
 
